5b9655d1232d4552aaf6c1017977277b5d714e90
olga20011 <olgaestherakello@gmail.com>
2021-03-21 17:49:22 +0300

    assignment 2

---
+++ Olga.js
@@ -0,0 +1,37 @@
+function getGrade(marks){
+    var marks=85
+    var grade=50
+    if (marks>=80){
+        grade ='A'
+        console.log(grade);
+    
+    } else if(marks>=70){
+        grade = 'B' 
+        console.log(grade);
+
+    }else if(marks>=60){
+        grade = 'C'
+        console.log(grade);
+
+    }else if(marks>=50){
+        grade = 'D'
+        console.log(grade);
+
+    }else if (marks<=49){
+        grade = 'E'
+        console.log(grade);
+
+    }else if (marks>=anything){
+        grade = 'F'
+        console.log(grade);
+    }
+    }
+     getGrade()
+    
+
+
+
+    
+
+
+  
